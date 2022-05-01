034a60a153cc4c4fd7587dd5fe58b37e4d93f13a
rahulr41180 <www.rahulr41180@gmail.com>
2022-05-01 12:48:59 +0530

    React Form Assignment Update

---
+++ unit-5/sprint-2/day-4/assignments/reactforms/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import { UserForm } from "./components/UserForm";
+import { UserList } from "./components/UserList";
+function App() {
+  return (
+    <div className="App">
+      <UserForm />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
